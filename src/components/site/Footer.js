57c6056dca87f1f1f4e9a89cfb8bb1243d0d74b3
Jordan Davis <jbd.profdev@gmail.com>
2023-11-07 16:18:25 -0500

    Added header, sidebar, and footer to respective files, along with some styling for each component.

---
+++ src/components/site/Footer.js
@@ -0,0 +1,11 @@
+import { Row } from "reactstrap";
+
+const Footer = () => {
+    return(
+        <footer>
+            <Row>
+                <p> Eleven Fifty 2023</p>
+            </Row>
+        </footer>
+    );
+};
