6510141765776f467e1755b40a6d528904cef969
Jordan Davis <jbd.profdev@gmail.com>
2023-11-08 15:08:26 -0500

    added classComponentDemo to sidebar

--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -3,9 +3,11 @@ import {
     Link,
     Routes
 } from 'react-router-dom';
+
 import Home from './Home';
 import Resources from './Resources';
 import FunctionalComponentDemo from '../concepts/FunctionalComponents/FunctionalComponentDemo';
+import ClassComponentDemo from '../concepts/ClassComponents/ClassComponentDemo';
 
 const Sidebar = () => {
     return(
@@ -15,6 +17,7 @@ const Sidebar = () => {
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/resources'>Resources</Link></li>
                     <li><Link to='/functionalcomponent'>Functional Component</Link></li>
+                    <li><Link to='/classcomponent'>Class Component</Link></li>
                 </ul>
             </div>
             <div className='sidebar-route'>
@@ -23,6 +26,7 @@ const Sidebar = () => {
                     <Route exact path='/resources' element={<Resources/>}/>
                     <Route exact path='/' element={<Home/>}/>
                     <Route exact path='/functionalcomponent' element={<FunctionalComponentDemo/>}/>
+                    <Route exact path='/classcomponent' element={<ClassComponentDemo/>}/>
                 </Routes>
             </div>
         </div>
