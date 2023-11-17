89de82cbee871ea3f742b078fb1c1c83b1206536
Jordan Davis <jbd.profdev@gmail.com>
2023-11-17 11:15:44 -0500

    added number and counter to props

--- src/components/concepts/Props/Props.js
+++ src/components/concepts/Props/Props.js
@@ -8,14 +8,21 @@ class Props extends React.Component {
             descriptionOne: 'Props give us the ability to pass data from a parent down to a child component.  Data within React flows uni-directionally, which means we can only pass props down to any subsequent child components called by the parent.',
             descriptionTwo: 'To send props down to a child component, you simply add an attribute to the call of the component that needs to receive the data.',
             descriptionThree: 'Whether you declare a component as a class component or a functional component, it must never modify its own props.',
+            number: 0
         }
+    }
+
+    counter = () => {
+        this.setState({
+            number: this.state.number + 1
+        })
     }
 
     render() {
         return(
             <div className="main">
                 <div className="mainDiv">
-                    <ChildComponent descriptionOne={this.state.descriptionOne} descriptionTwo={this.state.descriptionTwo} descriptionThree={this.state.descriptionThree}/>
+                    <ChildComponent descriptionOne={this.state.descriptionOne} descriptionTwo={this.state.descriptionTwo} descriptionThree={this.state.descriptionThree} number={this.state.number} counter={this.counter}/>
                 </div>
             </div>
         )
